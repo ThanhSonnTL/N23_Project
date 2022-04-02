4e7b3a1580eb7bbc0ed4a8c55826d1348fd158af
hiepb5222 <1951060695@e.tlu.edu.vn>
2022-04-02 20:31:26 +0700

    commit code

---
+++ app/src/main/java/com/example/Project_Nhom23/Login/SignUpActivity.java
@@ -0,0 +1,37 @@
+package com.example.Project_Nhom23.Login;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.kiemtra.R;
+
+public class SignUpActivity extends AppCompatActivity {
+    private EditText editTextemail,editTextname,editTextnumber,editTextpw,editTextconfirm;
+    private Button button_signup;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+        editTextemail=findViewById(R.id.editTextTextEmailAddress);
+        editTextpw=findViewById(R.id.editTextTextPassword);
+        editTextconfirm=findViewById(R.id.editTextTextConFirmPassword);
+        editTextname=findViewById(R.id.editTextTextName);
+        editTextnumber=findViewById(R.id.editTextTextNumber);
+
+    button_signup.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Intent intent=new Intent(SignUpActivity.this,LoginActivity.class);
+            startActivity(intent);
+        }
+    });
+
+    }
+}
